580b2cf44a25e0087da95a9749f0a27fe0f60eba
Nicklas Molving <nicklasmolving@gmail.com>
2016-11-29 12:47:17 +0100

    Revert "added: "
    This reverts commit b715a59bd05b713f342f49e6a18ce4e67982b45b.

--- src/test/java/AirlineEngineTests/CreateAirrouteRNGTest.java
+++ src/test/java/AirlineEngineTests/CreateAirrouteRNGTest.java
@@ -13,13 +13,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Ignore;
 
 /**
  *
  * @author Michael
  */
-@Ignore
 public class CreateAirrouteRNGTest {
 
     private static AirlineEngine airlineEngine;
